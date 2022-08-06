bf9c1279fa95873e7ab83118ae93624ea28839ee
zinx110 <tasdid110@gmail.com>
2022-08-06 14:21:16 +0600

    Add project files.

---
+++ oop2_c_sharp_supermarket_management_windowsform/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop2_c_sharp_supermarket_management_windowsform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
